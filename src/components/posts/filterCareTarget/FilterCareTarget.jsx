e9e3c9df8f6a99e8ef46992f7c6a4fdfeac613c9
Dawon Shin <shindw0314@gmail.com>
2023-11-20 18:56:03 +0900

    feat: add css module

--- src/components/posts/filterCareTarget/FilterCareTarget.jsx
+++ src/components/posts/filterCareTarget/FilterCareTarget.jsx
@@ -8,13 +8,15 @@ const careTargets = ['아동', '노인', '장애인'];
 const FilterCareTarget = () => {
   // const recruitingPost = [];
   return (
-    <div className={cx('filter-container')}>
-      {careTargets.map((target) => (
-        <label className={cx('filter-target')}>
-          <input type="checkbox" value={target} />
-          {target}
-        </label>
-      ))}
+    <div className={cx('wrapper')}>
+      <div className={cx('filter-container')}>
+        {careTargets.map((target) => (
+          <label className={cx('filter-target')}>
+            <input type="checkbox" value={target} />
+            {target}
+          </label>
+        ))}
+      </div>
     </div>
   );
 };
