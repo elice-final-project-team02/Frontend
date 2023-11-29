4a9cf0d82b27bfcff31063cdff383a061e9d3e53
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-11-29 14:22:19 +0900

    Feat: logout, PrivateRoute 수정

--- src/components/index.js
+++ src/components/index.js
@@ -26,3 +26,4 @@ export * from './common/pagination';
 export * from './user/authInput';
 export * from './user/authSelect';
 export { default as UserVerifiedApp } from './UserVerifiedApp';
+export { default as AuthProvider } from './AuthProvider';
