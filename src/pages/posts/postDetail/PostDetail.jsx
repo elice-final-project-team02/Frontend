335be0edecd3ce0aefa239ee769fcbaad1578dfd
Dawon Shin <shindw0314@gmail.com>
2023-12-02 03:26:43 +0900

    fix: fix layout and image

--- src/pages/posts/postDetail/PostDetail.jsx
+++ src/pages/posts/postDetail/PostDetail.jsx
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './PostDetail.module.scss';
 import cs from 'classnames/bind';
-import { FiTrash } from 'react-icons/fi';
 import { MdLocationOn, MdWatchLater } from 'react-icons/md';
 import { AiFillCalendar } from 'react-icons/ai';
 import { IoMdPerson } from 'react-icons/io';
 import { PiMoneyFill, PiTrashFill } from 'react-icons/pi';
 import { BiSolidPencil } from 'react-icons/bi';
-import { Child } from 'assets/images';
+import { Child, Senior1, Challenged } from 'assets/images';
 import { Link } from 'react-router-dom';
 import * as date from 'lib';
-import { useGetUser, useDeletePostAndGoHome, useGetRequestGoHome } from 'hooks';
 import * as data from 'lib';
+import { useGetUser, useDeletePostAndGoHome, useGetRequestGoHome } from 'hooks';
 import MessageForm from 'components/common/message/MessageForm.jsx';
 import { LoadingModal } from 'components';
 const cx = cs.bind(styles);
@@ -58,6 +57,12 @@ export default function PostDetail({ setMessageBoxState, setChatId }) {
       });
     }
   }, [requestData]);
+
+  const currentCareTarget = cx('target-image-wrapper', {
+    child: displayData.careTarget === '아동',
+    senior: displayData.careTarget === '노인',
+    disabled: displayData.careTarget === '장애인',
+  });
 
   function handleDeletePost() {
     if (window.confirm('게시물을 삭제하시겠습니까?')) {
@@ -256,19 +261,25 @@ export default function PostDetail({ setMessageBoxState, setChatId }) {
         >
           <div className={cx('even-columns')}>
             <div className={cx('features-wrapper')}>
-              <p>
+              <div className={cx('features')}>
                 <span>돌봄 대상 특징</span>
-                <span>{displayData.targetFeatures}</span>
-              </p>
-              <p>
+                <p>{displayData.targetFeatures}</p>
+              </div>
+              <div className={cx('features')}>
                 <span>돌봄 대상 유의사항</span>
-                <span>{displayData.cautionNotes}</span>
-              </p>
+                <p>{displayData.cautionNotes}</p>
+              </div>
             </div>
           </div>
           <div className={cx('even-columns')}>
             <span className={cx('target-image-wrapper')}>
-              <img src={Child} alt="" />
+              <img
+                src={
+                  displayData.careTarget === '아동' ? Child : displayData.careTarget === '노인' ? Senior1 : Challenged
+                }
+                alt="targetImage"
+                className={cx('target-image')}
+              />
             </span>
           </div>
         </div>
